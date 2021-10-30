edc8af6690fa935c8e710527c4a9a7633026c2c0
ham-zah <90850092+ham-zah@users.noreply.github.com>
2021-10-30 20:14:54 +0700

    Create script1.js

---
+++ _includes/script1.js
@@ -0,0 +1,9 @@
+var url_h = "https://webtools.my.id/";
+
+/* Set Logo Images */
+setTimeout(function() {
+    document.getElementById("logo").src = url_h + "img/logo.png"
+}, 5e3);
+setTimeout(function() {
+    document.getElementById("logo").src = url_h + "img/logo.svg"
+}, 1e4);
